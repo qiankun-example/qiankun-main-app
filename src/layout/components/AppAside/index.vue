0d7d8af9a001600644dded7050f729b6cebe2317
15870033509 <132010042@qq.com>
2020-11-29 10:48:07 +0800

    fix: aside

--- src/layout/components/AppAside/index.vue
+++ src/layout/components/AppAside/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <el-aside class="aside" :class="{collapse: $store.state.isCollapse}" style="width: auto" :style="{backgroundColor: variables.sidebarBg}">
-    <a href="/" class="logo">
-      <img src="http://eduboss.lagou.com/edu-boss-fed/assets/img/logo.e8b9190b.png" alt="edu">
+  <el-aside
+    class="aside"
+    :class="{ collapse: $store.state.isCollapse }"
+    style="width: auto"
+    :style="{ backgroundColor: '#304156' }"
+  >
+    <a href="/" class="logo" :style="{ backgroundColor: variables.sidebarBg }">
+      <img
+        src="http://eduboss.lagou.com/edu-boss-fed/assets/img/logo.e8b9190b.png"
+        alt="edu"
+      />
       <h1>Edu Boss</h1>
     </a>
+    <el-scrollbar class="menu-scollbar">
     <el-menu
       @select="selectMenu"
       :collapse="$store.state.isCollapse"
@@ -11,72 +20,99 @@
       :background-color="variables.menuBg"
       :text-color="variables.menuText"
       :active-text-color="variables.menuTextActive"
-      >
-      <app-aside-item v-for="route in permission_routes" :key="route.id" :item="route"></app-aside-item>
+      :collapse-transition="false"
+    >
+      <app-aside-item
+        v-for="route in permission_routes"
+        :key="route.id"
+        :item="route"
+      ></app-aside-item>
     </el-menu>
+    </el-scrollbar>
   </el-aside>
 </template>
 <script>
-import { mapGetters } from 'vuex'
-import { routerGo } from '@/utils/routes'
-import variables from '@/styles/jsVariables.scss'
-import AppAsideItem from './app-aside-item.vue'
+import { mapGetters } from "vuex";
+import { routerGo } from "@/utils/routes";
+import variables from "@/styles/jsVariables.scss";
+import AppAsideItem from "./app-aside-item.vue";
 export default {
-  name: 'AppAside',
+  name: "AppAside",
   components: {
-    AppAsideItem
+    AppAsideItem,
   },
   data() {
     return {
-      variables
-    }
+      variables,
+    };
   },
   computed: {
-    ...mapGetters([
-      'permission_routes',
-    ]),
+    ...mapGetters(["permission_routes"]),
   },
   methods: {
     selectMenu(index) {
-      routerGo(index, 'premission');
+      routerGo(index, "premission");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.aside {
+  transition: width .28s;
+  .logo {
+    display: flex;
+    align-items: center;
+    padding: 15px 0 15px 20px;
+    line-height: 30px;
+    color: $primary-color;
+    text-decoration: none;
+    img {
+      margin-right: 10px;
+      width: 30px;
+    }
+    h1 {
+      display: inline-block;
+      transition: width 0.3s;
+      width: 106px;
+      white-space: nowrap;
+      overflow: hidden;
+      margin: 0;
+    }
+  }
+  .menu-scollbar {
+    min-height: calc(100vh - 60px);
+    ::v-deep .el-scrollbar__wrap {
+      overflow-x: hidden;
+    }
+  }
+  .el-menu {
+    &:not(.el-menu--collapse) {
+      width: 240px;
+    }
+  }
+  &.collapse {
+    .logo {
+      h1 {
+        width: 0;
+      }
+    }
+    ::v-deep .el-menu--collapse>.el-menu-item span {
+      height: 0;
+      width: 0;
+      overflow: hidden;
+      visibility: hidden;
+      display: inline-block;
+    }
+    ::v-deep .el-menu--collapse .el-submenu > .el-submenu__title > span {
+      height: 0;
+      width: 0;
+      overflow: hidden;
+      visibility: hidden;
+      display: inline-block;
+    }
+    ::v-deep .el-menu--collapse .el-submenu>.el-submenu__title .el-submenu__icon-arrow {
+      display: none;
     }
   }
 }
-</script>
-<style lang="scss" scoped>
-  .aside {
-    .logo {
-      display: flex;
-      align-items: center;
-      padding: 15px 0 15px 20px;
-      line-height: 30px;
-      color: $primary-color;
-      text-decoration: none;
-      img {
-        margin-right: 10px;
-        width: 30px;
-      }
-      h1 {
-        display: inline-block;
-        transition: width 0.3s;
-        width: 106px;
-        white-space: nowrap;
-        overflow: hidden;
-        margin: 0;
-      }
-    }
-    .el-menu {
-      min-height: calc(100vh - 60px);
-      &:not(.el-menu--collapse) {
-        width: 240px;
-      }
-    }
-    &.collapse {
-      .logo {
-        h1 {
-          width: 0;
-        }
-      }
-    }
-  }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,4 @@
 /**@type {import('@vue/cli-service').ProjectOptions} */
-
-const externals = {
-  'vue': 'Vue',
-  'axios': 'axios',
-  'element-ui': 'ELEMENT',
-  'vue-router': 'VueRouter',
-}
-const cdn = {
-  dev: {
-    css: [],
-    js: []
-  },
-  prd: {
-    css: [
-      // element-ui css
-      '//cdn.bootcdn.net/ajax/libs/element-ui/2.13.0/theme-chalk/index.css'
-    ],
-    js: [
-      // vue
-      '//cdn.bootcdn.net/ajax/libs/vue/2.6.10/vue.min.js',
-      // vue-router
-      '//cdn.bootcdn.net/ajax/libs/vue-router/3.0.6/vue-router.min.js',
-      // axios
-      '//cdn.bootcdn.net/ajax/libs/axios/0.18.1/axios.min.js',
-      // element-ui js
-      '//cdn.bootcdn.net/ajax/libs/element-ui/2.13.0/index.js'
-    ]
-  }
-}
 module.exports = {
   outputDir: 'main_app',
   css: {
@@ -38,9 +9,30 @@ module.exports = {
     }
   },
   chainWebpack: config => {
+    const cdn = {
+      dev: {
+        css: [],
+        js: []
+      },
+      prd: {
+        css: [
+          // element-ui css
+          '//cdn.bootcdn.net/ajax/libs/element-ui/2.13.0/theme-chalk/index.css'
+        ],
+        js: [
+          // vue
+          '//cdn.bootcdn.net/ajax/libs/vue/2.6.10/vue.min.js',
+          // vue-router
+          '//cdn.bootcdn.net/ajax/libs/vue-router/3.0.6/vue-router.min.js',
+          // axios
+          '//cdn.bootcdn.net/ajax/libs/axios/0.18.1/axios.min.js',
+          // element-ui js
+          '//cdn.bootcdn.net/ajax/libs/element-ui/2.13.0/index.js'
+        ]
+      }
+    }
     config.plugins.delete('preload') // TODO: need test
     config.plugins.delete('prefetch') // TODO: need test
-
     // 设置html
     config.plugin('html')
       .tap(args => {
@@ -48,10 +40,13 @@ module.exports = {
         return args
       })
   },
-  configureWebpack: function (config) {
-    if (process.env.NODE_ENV === 'production') {
-      config.externals = externals
-    }
+  configureWebpack: {
+    externals: process.env.NODE_ENV === 'production' ? {
+      'vue': 'Vue',
+      'axios': 'axios',
+      'element-ui': 'ELEMENT',
+      'vue-router': 'VueRouter',
+    } : {}
   },
   devServer: {
     proxy: {
